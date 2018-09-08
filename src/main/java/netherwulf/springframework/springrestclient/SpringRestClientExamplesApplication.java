307971bbdf81f36db46e11fd4dd93e36c161f442
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-09 01:56:59 +0200

    Added POJOs for data model
    Added POJOs for data model

---
+++ src/main/java/netherwulf/springframework/springrestclient/SpringRestClientExamplesApplication.java
@@ -0,0 +1,12 @@
+package netherwulf.springframework.springrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+	}
+}
