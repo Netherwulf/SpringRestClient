848d7e8c444c5af6a59087fc49db02dc8eb5b4a7
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-09 23:07:28 +0200

    Added example reactive WebFlux WebClient
    Added example reactive WebFlux WebClient

--- src/main/java/netherwulf/springframework/springrestclient/services/ApiService.java
+++ src/main/java/netherwulf/springframework/springrestclient/services/ApiService.java
@@ -1,10 +1,14 @@
 package netherwulf.springframework.springrestclient.services;
 
 import netherwulf.springframework.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
 
     public List<User> getUsers(Integer limit);
+
+    public Flux<User> getUsers(Mono<Integer> limir);
 }
